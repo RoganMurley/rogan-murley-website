b1409d89874f52f3b0758ab83debfbf944ba7189
Rogan Murley <rogan@potatolondon.com>
2015-06-20 22:15:50 +0100

    Added partial template loading, removed accidentaly added files

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -2,6 +2,7 @@
     'use strict';
 
     var angular = require('angular');
+    var angularUIRouter = require('angular-ui-router');
 
     // Bootstrap data.
     var bootstrap = document.getElementById('bootstrap');
@@ -11,14 +12,31 @@
     };
 
     // Angular app.
-    var app = angular.module('landing', []);
+    var app = angular.module('landing', ['ui.router']);
 
-    app.config(/*@ngInject*/ function($interpolateProvider) {
+    app.config(/*@ngInject*/ function(
+        $interpolateProvider,
+        $locationProvider,
+        $stateProvider
+    ) {
         $interpolateProvider.startSymbol('[[').endSymbol(']]');
+        $locationProvider.html5Mode({
+            enabled: true
+        });
+        $stateProvider
+            .state('landing', {
+                templateUrl: staticUrl('partials/landing.html'),
+                url: '/',
+            });
     });
 
-    app.run( /*@ngInject*/ function ($rootScope) {
+    app.run( /*@ngInject*/ function (
+        $rootScope,
+        $state
+    ) {
         $rootScope.staticUrl = staticUrl;
         $rootScope.greeting = 'Hello!';
+
+        $state.transitionTo('landing');
     });
 }());

af7e0f5bb3585d0ffd69fc96039c678ca20c34cc
Rogan Murley <rogan@potatolondon.com>
2015-06-21 13:43:53 +0100

    Added background images to tiles

--- frontend/src/js/components/tileDirective.js
+++ frontend/src/js/components/tileDirective.js
@@ -3,9 +3,17 @@
 
     var TileDirective = /*@ngInject*/ function ($rootScope) {
         return {
+            bindToController: true,
+            controller: /*@ngInject*/ function ($rootScope) {
+                this.imageURL = $rootScope.staticUrl(this.image);
+            },
+            controllerAs: 'ctrl',
             require: '^tiles',
             restrict: 'E',
-            scope: {},
+            scope: {
+                image: '@',
+                imageAlt: '@'
+            },
             templateUrl: $rootScope.staticUrl('partials/components/tile.html'),
             transclude: true
         };
